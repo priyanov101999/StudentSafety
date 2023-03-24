c603353d8936e647f6649b5ed246eb5051ff828d
priyanov101999 <priyanov101999@gmail.com>
2023-03-24 18:38:45 +0530

    build script added

---
+++ scripts/run.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+cd /absolute/path/to/my/project
+export NODE_ENV=development
+npm start
